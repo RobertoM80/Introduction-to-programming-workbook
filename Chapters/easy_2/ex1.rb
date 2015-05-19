61a6a6aaf5a7577a7827298240aac9b28f400bf4
Roberto Mirabella <bronzetto@outlook.com>
2015-05-19 21:56:55 +0100

    Added ex1.rb to Quiz easy_2

---
+++ Chapters/easy_2/ex1.rb
@@ -0,0 +1,14 @@
+#In this hash of people and their age,
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+#see if there is an age present for "Spot".
+
+#Bonus:
+
+#What are two other hash methods that would work just as well for this solution?
+
+p ages.key?("Spot")
+p ages.has_key?("Spot")
+p ages.include?("Spot")
+p ages.member?("Spot")
