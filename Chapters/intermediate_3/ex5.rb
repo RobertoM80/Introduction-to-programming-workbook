b10add10208f2de7fc91f0a2cedb8db916ac866b
Roberto Mirabella <bronzetto@outlook.com>
2015-05-23 13:32:36 +0100

    Added ex5.rb to Quiz intermediate_3

---
+++ Chapters/intermediate_3/ex5.rb
@@ -0,0 +1,14 @@
+#How could the unnecessary duplication in this method be removed?
+
+def color_valid
+  if color == "blue" || color == "green"
+    true
+  else
+    false
+  end
+end
+
+
+def color_valid 
+  color == "blue" || color == "green"
+end 
